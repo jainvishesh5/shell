990f40bfd98f308246bbda121575d2b236ca6e62
vishesh jain <vishesh.yahoo.com@gmail.com>
2026-05-25 04:16:33 +0530

    implemented repl and added echo ,  exit

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,20 @@
  int main(){
     cout << unitbuf;
     cerr << unitbuf;
-
+string command;
+  while(true){
     cout << "$ ";
+   
+    getline(cin , command);
+     
+    if(command == "exit"){
+    break;
+  }
+    if(command.rfind("echo " , 0)==0){
+    cout << command.substr(5) << "\n";
+    continue;
+  }
+
+   cout << command << ": command not found" << "\n";
+  }
 }
